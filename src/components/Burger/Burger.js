7fbe6bf25b707caa3bd5127b531c3f71bbf8fdaa
nehaa22 <nehapalekar026@gmail.com>
2021-01-16 18:18:14 +0530

    129 - Burger - added map to get ingredient array from object

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,16 @@ import classes from './Burger.module.css'
 import BurgerIngredient from "./BurgerIngredients/BurgerIngredients";
 
 const Burger = (props) => {
+    const transformedIngredients=Object.keys(props.ingredients)
+    .map(igkey =>{
+        return[...Array(props.ingredients[igkey])].map((_,i) =>{
+            return <BurgerIngredient key={igkey+1} type={igkey}/>
+        });
+    });
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="meat" />
-            <BurgerIngredient type="cheese" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
