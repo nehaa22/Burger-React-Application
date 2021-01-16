3c89de0c89f9b63240cefd7c6e0fb1be5444d3f1
nehaa22 <nehapalekar026@gmail.com>
2021-01-16 18:30:51 +0530

    130 - Burger - added reduce to get dynamically added ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,20 @@ import classes from './Burger.module.css'
 import BurgerIngredient from "./BurgerIngredients/BurgerIngredients";
 
 const Burger = (props) => {
-    const transformedIngredients=Object.keys(props.ingredients)
+    let transformedIngredients=Object.keys(props.ingredients)
     .map(igkey =>{
         return[...Array(props.ingredients[igkey])].map((_,i) =>{
             return <BurgerIngredient key={igkey+1} type={igkey}/>
         });
-    });
+    })
+    .reduce((arr,el) =>{
+        return arr.concat(el)
+    },[]);
+
+    if(transformedIngredients.length ===0){
+        transformedIngredients = <p>Please start adding Ingredients</p>
+    }
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
