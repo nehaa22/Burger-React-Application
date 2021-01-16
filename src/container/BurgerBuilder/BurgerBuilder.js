b7d5688230950140af3719b079c1bf3b1573496a
nehaa22 <nehapalekar026@gmail.com>
2021-01-16 17:45:23 +0530

    129 - added ingredient state as array inside NurgerBuilder

--- src/container/BurgerBuilder/BurgerBuilder.js
+++ src/container/BurgerBuilder/BurgerBuilder.js
@@ -2,11 +2,19 @@ import React, { Component } from "react";
 import Burger from "../../components/Burger/Burger";
 import Aux from "../../Hoc/Aux";
 
-class BurgerBuilder extends Component{
-    render(){
-        return(
+class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
+    render() {
+        return (
             <Aux>
-                <Burger/>
+                <Burger />
                 <div>Build Controls</div>
             </Aux>
 
