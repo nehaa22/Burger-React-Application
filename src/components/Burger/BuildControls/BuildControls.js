402bf60807b6dcca803ffc47102e8c0f79c7e715
nehaa22 <nehapalekar026@gmail.com>
2021-01-17 12:18:28 +0530

    132 - BuildControls - added controls array of ingredients

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,5 +1,11 @@
 import React from "react";
 
+const controls =[
+    {label:'Salad', type : 'salad'},
+    {label:'Bacon', type : 'bacon'},
+    {label:'Cheese', type : 'cheese'},
+    {label:'Meat', type : 'meat'}
+]
 const BuildControls = (prop) =>{
     return <div></div>
 
