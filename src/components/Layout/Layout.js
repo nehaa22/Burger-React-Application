ba6542dee20a6c22251cc88fd8c9094be7b7aaf7
nehaa22 <nehapalekar026@gmail.com>
2021-01-18 18:35:52 +0530

    146 - create SideDrawer and added in layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Aux from '../../Hoc/Aux';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import classes from './Layout.module.css';
 
 const Layout = (props) => (
     <Aux>
         <Toolbar/>
+        <SideDrawer/>
         <main className={classes.Content}>
             {props.children}
         </main>
---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from "./SideDrawer.module.css";
+
+const SideDrawer = () =>{
+    return(
+        <div className = {classes.SideDrawer}>
+            <Logo/>
+            <nav>
+                <NavigationItems/>
+            </nav>
+        </div>
+    )
+}
+
+export default SideDrawer;
