81fb2a0fb0f85e2010fd9fa4df1783def250181b
nehaa22 <nehapalekar026@gmail.com>
2021-01-19 10:24:27 +0530

    148 - More about Responsive Adjustment

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -6,7 +6,9 @@ import classes from "./SideDrawer.module.css";
 const SideDrawer = () =>{
     return(
         <div className = {classes.SideDrawer}>
+            <div className={classes.Logo}>
             <Logo/>
+            </div>
             <nav>
                 <NavigationItems/>
             </nav>
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -6,8 +6,10 @@ import classes from './Toolbar.module.css'
 const Toolbar = (props) =>(
     <header className = {classes.Toolbar}>
         <div>MENU</div>
+        <div className = {classes.Logo}>
         <Logo/>
-        <nav>
+        </div>
+        <nav className={classes.DesktopOnly}>
             <NavigationItems/>
         </nav>
     </header>
