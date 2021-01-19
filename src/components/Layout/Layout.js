8fea6043aa1c03b4a3654c416105846c4eb2be4a
nehaa22 <nehapalekar026@gmail.com>
2021-01-19 16:27:08 +0530

    149 - converted Layout in class component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../Hoc/Aux';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import classes from './Layout.module.css';
 
-const Layout = (props) => (
-    <Aux>
-        <Toolbar/>
-        <SideDrawer/>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
+    render() {
+        return (
+            <Aux>
+                <Toolbar />
+                <SideDrawer />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        );
+    };
+}
 
 export default Layout;
