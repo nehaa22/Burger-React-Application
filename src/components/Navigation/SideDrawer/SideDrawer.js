e7fef2e5b26e46d988b349c6104ad2322565ddaf
nehaa22 <nehapalekar026@gmail.com>
2021-01-19 16:22:44 +0530

    149 - added Backdrop in sidedrawer

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import Aux from '../../../Hoc/Aux';
 import Logo from '../../Logo/Logo';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from "./SideDrawer.module.css";
 
-const SideDrawer = () =>{
-    return(
-        <div className = {classes.SideDrawer}>
-            <div className={classes.Logo}>
-            <Logo/>
+const SideDrawer = () => {
+    return (
+        <Aux>
+            <Backdrop show />
+            <div className={classes.SideDrawer}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems/>
-            </nav>
-        </div>
+        </Aux>
     )
 }
 
