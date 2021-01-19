ddd287a4a03a717a2c0955f8c53e614ca9c48eef
nehaa22 <nehapalekar026@gmail.com>
2021-01-19 23:09:18 +0530

    154 - Convert OrderSummary into class component-

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,27 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../Hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const OrderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(igKey => {
-            return (<li key={igKey}>
-                <span style={{ textTransform: 'capitalize' }}>{igKey}</span> : {props.ingredients[igKey]}
-            </li>)
-        });
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with following ingredients</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p> <strong>Total Price : {props.price}</strong></p>
-            <p>Continue to checkout ?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-        </Aux>
-    )
-};
+class OrderSummary extends Component {
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+            .map(igKey => {
+                return (<li key={igKey}>
+                    <span style={{ textTransform: 'capitalize' }}>{igKey}</span> : {this.props.ingredients[igKey]}
+                </li>)
+            });
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with following ingredients</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p> <strong>Total Price : {this.props.price}</strong></p>
+                <p>Continue to checkout ?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </Aux>
 
+        )
+    }
+}
 export default OrderSummary;
