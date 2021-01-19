edd3d06e338d0ff7c5cea44acf7f6b885ed0496a
nehaa22 <nehapalekar026@gmail.com>
2021-01-19 23:34:11 +0530

    154 - Modal - added shouldUpdate and DidUpdate method

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,22 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import Aux from "../../../Hoc/Aux";
 import Backdrop from "../Backdrop/Backdrop";
 import classes from './Modal.module.css';
 
+class Modal extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    }
+    componentDidUpdate(){
+        console.log("[Modal] - Did update");
+    }
 
 
-const Modal = (props) => (
-    <Aux>
-        <Backdrop show = {props.show} clicked = {props.modalClosed}/>
-    <div 
-    className={classes.Modal}
-    style = {{
-        transform : props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity : props.show ? '1': '0'
-    }}>
-        {props.children}
-    </div>
-    </Aux>
-);
+    render() {
+        return (
+
+            <Aux>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+                <div
+                    className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}>
+                    {this.props.children}
+                </div>
+            </Aux>
+        )
+    }
+}
 
 export default Modal;
